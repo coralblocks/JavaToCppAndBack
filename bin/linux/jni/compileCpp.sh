dd2c8f20e349c700665798dcc131c563c42c4716
CoralBlocks <132032478+coralblocks@users.noreply.github.com>
2023-05-05 11:03:21 -0300

    Changing from g++ to clang++

--- bin/linux/jni/compileCpp.sh
+++ bin/linux/jni/compileCpp.sh
@@ -2,5 +2,5 @@
 
 mkdir -p lib/jni
 
-g++ -std=c++20 -shared -fPIC -I${JAVA_HOME}/include -I${JAVA_HOME}/include/linux src/main/c/com_coralblocks_javatocppandback_jni_HelloWorld.cpp -o lib/jni/libHelloWorld.so
+clang++ -std=c++20 -shared -fPIC -I${JAVA_HOME}/include -I${JAVA_HOME}/include/linux src/main/c/com_coralblocks_javatocppandback_jni_HelloWorld.cpp -o lib/jni/libHelloWorld.so
 
--- bin/linux/jni_jvm/compileCpp.sh
+++ bin/linux/jni_jvm/compileCpp.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-g++ -std=c++20 -I"$JAVA_HOME/include" -I"$JAVA_HOME/include/linux" -o HelloWorld src/main/c/com_coralblocks_javatocppandback_jni_jvm_HelloWorld.cpp -L"$JAVA_HOME/lib/server" -ljvm -Wno-write-strings
+clang++ -std=c++20 -I"$JAVA_HOME/include" -I"$JAVA_HOME/include/linux" -o HelloWorld src/main/c/com_coralblocks_javatocppandback_jni_jvm_HelloWorld.cpp -L"$JAVA_HOME/lib/server" -ljvm -Wno-write-strings
 
