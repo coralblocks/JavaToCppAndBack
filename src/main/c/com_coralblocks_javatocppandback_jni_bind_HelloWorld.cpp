07a808688832b290a6b3f4672035943ab5791e04
CoralBlocks <132032478+coralblocks@users.noreply.github.com>
2023-05-05 09:47:25 -0300

    Organization for JNI-BIND

--- src/main/c/com_coralblocks_javatocppandback_jni_bind_HelloWorld.cpp
+++ src/main/c/com_coralblocks_javatocppandback_jni_bind_HelloWorld.cpp
@@ -1,18 +1,10 @@
 #include "jni_bind_release.h"
 #include "com_coralblocks_javatocppandback_jni_bind_HelloWorld.h"
 
-// 1: Setup JNI Bind in your OnLoad call (needed only once).
+// required for JNI-BIND:
 std::unique_ptr < jni::JvmRef < jni::kDefaultJvm >> jvm;
 
-static constexpr jni::Class kClass {
-    "com/coralblocks/javatocppandback/jni_bind/HelloWorld",
-    jni::Method {
-        "sayHello",
-        jni::Return < void > {},
-        jni::Params < jint , jstring> {}
-    }
-};
-
+// required for JNI-BIND:
 extern "C" {
     JNIEXPORT jint JNICALL JNI_OnLoad(JavaVM * pjvm, void * ) {
         jvm.reset(new jni::JvmRef < jni::kDefaultJvm > (pjvm));
@@ -20,15 +12,35 @@ extern "C" {
     }
 }
 
-JNIEXPORT void JNICALL Java_com_coralblocks_javatocppandback_jni_1bind_HelloWorld_goToNativeSide
-  (JNIEnv *env, jclass klass, jint x, jstring msg) {
-
-    jni::LocalObject < kClass > helloWorld {}; // Constructs new instance.
-    helloWorld("sayHello", x, msg);
-}
-
+// required for JNI-BIND:
 JNIEXPORT void JNICALL Java_com_coralblocks_javatocppandback_jni_1bind_HelloWorld_tearDownJvm
   (JNIEnv *, jclass) {
 	jvm = nullptr; // release before exiting the JVM on the Java side...
 }
+
+JNIEXPORT void JNICALL Java_com_coralblocks_javatocppandback_jni_1bind_HelloWorld_goToNativeSide
+  (JNIEnv *env, jclass klass, jint x, jstring msg) {
+
+    // using pure JNI:
+    /*
+    jclass helloWorldClass = env->FindClass("com/coralblocks/javatocppandback/jni_bind/HelloWorld");
+    jmethodID helloWorldConstructor = env->GetMethodID(helloWorldClass, "<init>", "()V");
+    jobject helloWorldObj = env->NewObject(helloWorldClass, helloWorldConstructor);
+    jmethodID sayHelloMethod = env->GetMethodID(helloWorldClass, "sayHello", "(ILjava/lang/String;)V");
+    env->CallVoidMethod(helloWorldObj, sayHelloMethod, x, msg);
+    */
+
+   // using JNI-BIND:
+   static constexpr jni::Class kClass {
+      "com/coralblocks/javatocppandback/jni_bind/HelloWorld",
+      jni::Method {
+          "sayHello",
+          jni::Return < void > {},
+          jni::Params < jint , jstring> {}
+      }
+    };
+    jni::LocalObject < kClass > helloWorld {}; // Constructs new instance.
+    helloWorld("sayHello", x, msg);
+}
+
 
