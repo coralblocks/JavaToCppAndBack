e90fa34986979091da27221533a33edfe10c0576
CoralBlocks <132032478+coralblocks@users.noreply.github.com>
2023-05-05 10:06:39 -0300

    Removing unnecessary -I from g++

--- bin/mac/jni_callback/compileCpp.sh
+++ bin/mac/jni_callback/compileCpp.sh
@@ -2,7 +2,7 @@
 
 mkdir -p lib/jni
 
-g++ -std=c++20 -shared -fPIC -I"$JAVA_HOME/include" -I"$JAVA_HOME/include/darwin/" -I./src/main/c src/main/c/com_coralblocks_javatocppandback_jni_callback_CallbackFromJavaSharedLib.cpp -o lib/jni/libCallbackFromJavaSharedLib.dylib
+g++ -std=c++20 -shared -fPIC -I"$JAVA_HOME/include" -I"$JAVA_HOME/include/darwin/" src/main/c/com_coralblocks_javatocppandback_jni_callback_CallbackFromJavaSharedLib.cpp -o lib/jni/libCallbackFromJavaSharedLib.dylib
 
-g++ -std=c++20 -I"$JAVA_HOME/include" -I"$JAVA_HOME/include/darwin" -I./src/main/c -o CallbackFromJava src/main/c/com_coralblocks_javatocppandback_jni_callback_CallbackFromJava.cpp -Llib/jni -lCallbackFromJavaSharedLib -L"$JAVA_HOME/lib/server" -ljvm -Wno-write-strings
+g++ -std=c++20 -I"$JAVA_HOME/include" -I"$JAVA_HOME/include/darwin" -o CallbackFromJava src/main/c/com_coralblocks_javatocppandback_jni_callback_CallbackFromJava.cpp -Llib/jni -lCallbackFromJavaSharedLib -L"$JAVA_HOME/lib/server" -ljvm -Wno-write-strings
 
